da2b0bb88a997344ce6314df38795e3520ba400a
LyhorngDeveloper2 <developer2@ione2u.com>
2025-01-16 14:56:10 +0700

    fix get username

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder,SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+  .setTitle('API Demo')
+  .setDescription('The API description')
+  .setVersion('1.0')
+  .addTag('example') // Optional: Group APIs by tags
+  .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();

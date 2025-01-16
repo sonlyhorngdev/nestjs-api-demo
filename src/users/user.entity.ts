6cb34d5680f7e740c8375eb320f076f8d9056b38
LyhorngDeveloper2 <developer2@ione2u.com>
2025-01-16 16:11:06 +0700

    implement auth

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -5,8 +5,11 @@ export class User {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column()
+  @Column({ unique: true })
   username: string;
+
+  @Column()
+  password: string;
 
   @Column()
   email: string;
